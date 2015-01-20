70a31b3f167ee8fbce5c1b600e58c129bc0e90e8
Juan Ignacio Vimberg <jivimberg@gmail.com>
2015-01-19 19:04:43 -0800

    All tests running succesffuly

--- library/test/com/orm/query/SelectTest.java
+++ library/test/com/orm/query/SelectTest.java
@@ -11,12 +11,12 @@ public class SelectTest {
     @Test
     public void testMergeCondition(){
         Select where = Select.from(TestRecord.class).where(Condition.prop("test").eq("satya"));
-        assertEquals("test = ? ", where.getWhereCond());
+        assertEquals("(test = ? )", where.getWhereCond());
         assertEquals(1, where.getArgs().length);
         assertEquals("satya", where.getArgs()[0]);
 
         where = Select.from(TestRecord.class).where(Condition.prop("test").eq("satya"), Condition.prop("prop").eq(2));
-        assertEquals("test = ?  AND prop = ? ", where.getWhereCond());
+        assertEquals("(test = ?  AND prop = ? )", where.getWhereCond());
         assertEquals(2, where.getArgs().length);
         assertEquals("satya", where.getArgs()[0]);
         assertEquals("2", where.getArgs()[1]);
@@ -26,12 +26,12 @@ public class SelectTest {
     @Test
     public void testWhere(){
         Select where = Select.from(TestRecord.class).where(Condition.prop("test").eq("satya"));
-        assertEquals("test = ? ", where.getWhereCond());
+        assertEquals("(test = ? )", where.getWhereCond());
         assertEquals(1, where.getArgs().length);
         assertEquals("satya", where.getArgs()[0]);
 
         where = Select.from(TestRecord.class).where(Condition.prop("test").eq("satya"), Condition.prop("prop").eq(2));
-        assertEquals("test = ?  AND prop = ? ", where.getWhereCond());
+        assertEquals("(test = ?  AND prop = ? )", where.getWhereCond());
         assertEquals(2, where.getArgs().length);
         assertEquals("satya", where.getArgs()[0]);
         assertEquals("2", where.getArgs()[1]);
@@ -41,12 +41,12 @@ public class SelectTest {
     @Test
     public void testWhereOr(){
         Select where = Select.from(TestRecord.class).whereOr(Condition.prop("test").eq("satya"));
-        assertEquals("test = ? ", where.getWhereCond());
+        assertEquals("(test = ? )", where.getWhereCond());
         assertEquals(1, where.getArgs().length);
         assertEquals("satya", where.getArgs()[0]);
 
         where = Select.from(TestRecord.class).whereOr(Condition.prop("test").eq("satya"), Condition.prop("prop").eq(2));
-        assertEquals("test = ?  OR prop = ? ", where.getWhereCond());
+        assertEquals("(test = ?  OR prop = ? )", where.getWhereCond());
         assertEquals(2, where.getArgs().length);
         assertEquals("satya", where.getArgs()[0]);
         assertEquals("2", where.getArgs()[1]);
@@ -55,13 +55,13 @@ public class SelectTest {
     @Test
     public void testAnd(){
         Select where = Select.from(TestRecord.class).whereOr(Condition.prop("test").eq("satya"));
-        assertEquals("test = ? ", where.getWhereCond());
+        assertEquals("(test = ? )", where.getWhereCond());
         assertEquals(1, where.getArgs().length);
         assertEquals("satya", where.getArgs()[0]);
 
         where.and(Condition.prop("prop").eq(2));
 
-        assertEquals("test = ?  AND prop = ? ", where.getWhereCond());
+        assertEquals("(test = ? ) AND (prop = ? )", where.getWhereCond());
         assertEquals(2, where.getArgs().length);
         assertEquals("satya", where.getArgs()[0]);
         assertEquals("2", where.getArgs()[1]);
@@ -70,13 +70,13 @@ public class SelectTest {
     @Test
     public void testOr(){
         Select where = Select.from(TestRecord.class).whereOr(Condition.prop("test").eq("satya"));
-        assertEquals("test = ? ", where.getWhereCond());
+        assertEquals("(test = ? )", where.getWhereCond());
         assertEquals(1, where.getArgs().length);
         assertEquals("satya", where.getArgs()[0]);
 
         where.or(Condition.prop("prop").eq(2));
 
-        assertEquals("test = ?  OR prop = ? ", where.getWhereCond());
+        assertEquals("(test = ? ) OR (prop = ? )", where.getWhereCond());
         assertEquals(2, where.getArgs().length);
         assertEquals("satya", where.getArgs()[0]);
         assertEquals("2", where.getArgs()[1]);
