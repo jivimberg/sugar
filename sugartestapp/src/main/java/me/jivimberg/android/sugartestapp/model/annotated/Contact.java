f5a1b59563130243d5b6ebf4c0437a7ac4a3f437
Juan Ignacio Vimberg <jivimberg@gmail.com>
2015-01-24 12:41:14 -0800

    Shaving off extra code

--- sugartestapp/src/main/java/me/jivimberg/android/sugartestapp/model/annotated/Contact.java
+++ sugartestapp/src/main/java/me/jivimberg/android/sugartestapp/model/annotated/Contact.java
@@ -43,44 +43,4 @@ public final class Contact {
     public boolean isVIP() {
         return isVIP;
     }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        Contact contact = (Contact) o;
-
-        if (age != contact.age) return false;
-        if (Double.compare(contact.height, height) != 0) return false;
-        if (isVIP != contact.isVIP) return false;
-        if (Float.compare(contact.weight, weight) != 0) return false;
-        if (name != null ? !name.equals(contact.name) : contact.name != null) return false;
-
-        return true;
-    }
-
-    @Override
-    public int hashCode() {
-        int result;
-        long temp;
-        result = name != null ? name.hashCode() : 0;
-        result = 31 * result + age;
-        result = 31 * result + (weight != +0.0f ? Float.floatToIntBits(weight) : 0);
-        temp = Double.doubleToLongBits(height);
-        result = 31 * result + (int) (temp ^ (temp >>> 32));
-        result = 31 * result + (isVIP ? 1 : 0);
-        return result;
-    }
-
-    @Override
-    public String toString() {
-        return "Contact{" +
-                "name='" + name + '\'' +
-                ", age=" + age +
-                ", weight=" + weight +
-                ", height=" + height +
-                ", isVIP=" + isVIP +
-                '}';
-    }
 }
--- sugartestapp/src/main/java/me/jivimberg/android/sugartestapp/model/annotated/Project.java
+++ sugartestapp/src/main/java/me/jivimberg/android/sugartestapp/model/annotated/Project.java
@@ -25,32 +25,4 @@ public class Project {
     public String getName() {
         return name;
     }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        Project project = (Project) o;
-
-        if (id != project.id) return false;
-        if (name != null ? !name.equals(project.name) : project.name != null) return false;
-
-        return true;
-    }
-
-    @Override
-    public int hashCode() {
-        int result = id;
-        result = 31 * result + (name != null ? name.hashCode() : 0);
-        return result;
-    }
-
-    @Override
-    public String toString() {
-        return "Project{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                '}';
-    }
 }
